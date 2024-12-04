d2243966f5b5e9bb03d9cc090b2d87ab4249f3a0
arthur <arthurdedsec@gmail.com>
2024-12-04 19:56:34 -0300

    back end tela de cadastro alunos

--- app/Livewire/Users/CreateUsers.php
+++ app/Livewire/Users/CreateUsers.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Users;
 
+use App\Models\alunos;
 use App\Models\User;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
@@ -11,22 +12,33 @@ class CreateUsers extends Component
 
     #[Validate('required', message: 'Preencha esse campo')]
 
+    public $first_name = '';
 
-    public $name = '';
 
-    #[Validate('required', message: )]
+    #[Validate('required', message: 'Preencha com o sobrenome')]
+
+    public $last_name = '';
+
+
+    #[Validate('required', message: 'Esse campo precisa ser preenchido')]
+    #[Validate('email', message: 'Esse campo precisa ser um email')]
+
     public $email = '';
 
-    #[Validate('required|string')]
+    #[Validate('required', message: 'Coloque o CPF do aluno')]
+    #[Validate('string', message: 'Coloque apenas numeros')]
+
     public $cpf;
 
-    #[Validate('required|date')]
+    #[Validate('required', message: 'preencha a data de nascimento do aluno')]
+    #[Validate('date', message: 'Esse campo precisa ser uma data')]
+    
     public $date_birth;
     
 
     public function create_user(){
         $validated = $this->validate();
-        User::create($validated);
+        alunos::create($validated);
 
         return redirect()->route('index.home');
     }
--- resources/views/livewire/users/create-users.blade.php
+++ resources/views/livewire/users/create-users.blade.php
@@ -10,25 +10,44 @@
         <div class="grid items-center justify-center grid-cols-2 gap-2 text-center">
             <div>
                 <label for="name">Nome do Cliente</label>
-                <input type="text" wire:model="name" id="name" class="border">
+                <input type="text" wire:model="first_name" id="name" class="border">
                 <div>
-                    @error('name') <span class="text-red-500 error">{{ $message }}</span> @enderror
+                    @error('first_name') <span class="text-red-500 error">{{ $message }}</span> @enderror
+                </div>
+            </div>
+
+
+            <div>
+                <label for="name">sobrenome do Cliente</label>
+                <input type="text" wire:model="last_name" id="name" class="border">
+                <div>
+                    @error('last_name') <span class="text-red-500 error">{{ $message }}</span> @enderror
                 </div>
             </div>
 
             <div>
                 <label for="email">Email Do Cliente</label>
                 <input type="email" wire:model="email" id="email" class="border">
+                <div>
+                    @error('email') <span class="text-red-500 error">{{ $message }}</span> @enderror
+                </div>
             </div>
 
             <div>
                 <label for="cpf">CPF Do Cliente</label>
                 <input type="text" wire:model="cpf" id="cpf" class="border">
+                <div>
+                    @error('cpf') <span class="text-red-500 error">{{ $message }}</span> @enderror
+                </div>
             </div>
 
             <div>
                 <label for="data_nascimento">Data De Nascimento Do Cliente</label>
                 <input type="date" wire:model="date_birth" id="data_nascimento">
+
+                <div>
+                    @error('date_birth') <span class="text-red-500 error">{{ $message }}</span> @enderror
+                </div>
             </div>
 
             <div class="col-span-2">
